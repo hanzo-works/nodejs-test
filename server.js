006a0be985d38b3216792b08445f2da6a8709c9e
hanzo-works <82083483+hanzo-works@users.noreply.github.com>
2021-04-07 18:11:31 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-// server.js
+//app.js
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const basicAuth = require('basic-auth-connect');
 
-var http = require('http')
-var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
-}).listen(port);
+app.use(basicAuth('n0bisuke', 'sugawara'));
+app.get('/', (req, res) => {
+  res.send('Hello World')
+});
+
+app.listen(PORT);
