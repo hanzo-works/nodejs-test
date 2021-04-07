8ff80c9ba18995d3d062797c336fb4c7f0086657
hanzo-works <82083483+hanzo-works@users.noreply.github.com>
2021-04-07 19:59:00 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Authentication module.
 const auth = require('http-auth');
 const authConnect = require('http-auth-connect');
-
+ 
 const basic = auth.basic({
     realm: "Simon Area.",
     file: __dirname + "/data/users.htpasswd"
@@ -10,10 +10,13 @@ const PORT = process.env.PORT || 3000;
 // Application setup.
 const express = require('express');
 const app = express();
-app.use(authConnect(basic));
+app.use('/admin/*',authConnect(basic));
+app.get('/admin/', (req, res) => {
+    res.send(`Hello from express - ${req.user}!`);
+});
 
 // Setup route.
 app.get('/', (req, res) => {
-    res.send(`Hello from express - ${req.user}!`);
+    res.send(`Hello !`);
 });
 app.listen(PORT);
